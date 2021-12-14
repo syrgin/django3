5ed212c861908c08df9d5c2d7f8319e1e907c71b
Aliaksandr Surhinevich <93003038+syrgin@users.noreply.github.com>
2021-12-14 03:30:11 +0300

    Initial commit

---
+++ coolsite/women/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+
+admin.site.register(Women, WomenAdmin)
---
+++ coolsite/women/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+
+class Women(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['-time_create', 'title']
+
+
+
